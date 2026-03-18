3e00c60afce58defe07b879d14a80fa946f1e3aa
Marco Ollivier <marco.mota@neon.com.br>
2026-03-18 15:20:59 -0300

    feat: ativar números de linha relativos (relativenumber)
    Com relativenumber ativo, cada linha mostra a distância em relação
    ao cursor em vez do número absoluto. Isso é fundamental para comandos
    de movimento vim: "5j" pula 5 linhas abaixo, "3k" sobe 3 etc.
    A linha do cursor continua mostrando o número absoluto (number=true).

--- init.lua
+++ init.lua
@@ -100,9 +100,8 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+-- Números relativos: permite pular linhas com 5j, 3k etc.
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
